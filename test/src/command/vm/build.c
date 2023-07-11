d0353662ba86e229bafab6091afa2f6eff36941a
David Steele <david@pgmasters.net>
2023-07-11 18:54:41 +0300

    Working on pg rendering.

--- test/src/command/vm/build.c
+++ test/src/command/vm/build.c
@@ -138,7 +138,7 @@ Render vm script
 typedef struct RenderData
 {
     bool debug;                                                     // In debug mode?
-    bool command;                                                   // Last added was a command
+    bool commandLast;                                               // Last added was a command
     String *script;                                                 // Script
 
 } RenderData;
@@ -152,6 +152,12 @@ vmRenderTitle(RenderData *const data, const char *const title)
         FUNCTION_TEST_PARAM(STRINGZ, title);
     FUNCTION_TEST_END();
 
+    if (data->commandLast)
+    {
+        strCatChr(data->script, '\n');
+        data->commandLast = false;
+    }
+
     if (!strEmpty(data->script))
         strCatChr(data->script, '\n');
 
@@ -160,13 +166,37 @@ vmRenderTitle(RenderData *const data, const char *const title)
     FUNCTION_TEST_RETURN_VOID();
 }
 
+// Helper to render a command
+static void
+vmRenderCommand(RenderData *const data, const char *const command)
+{
+    FUNCTION_TEST_BEGIN();
+        FUNCTION_TEST_PARAM_P(VOID, data);
+        FUNCTION_TEST_PARAM(STRINGZ, command);
+    FUNCTION_TEST_END();
+
+    if (data->commandLast)
+    {
+        strCatChr(data->script, '\n');
+    }
+
+    strCatFmt(data->script, "RUN %s", command);
+    data->commandLast = true;
+
+    FUNCTION_TEST_RETURN_VOID();
+}
+
 String *
-vmRender(const VmDefinition *const vmDef, const bool debug, const StringList *const packageList)
+vmRender(
+    const VmDefinition *const vmDef, const bool debug, const StringList *const packageList, const StringList *const pgList,
+    const String *const pgBeta)
 {
     FUNCTION_LOG_BEGIN(logLevelDebug);
         FUNCTION_LOG_PARAM(STRING, vmDef->id);
         FUNCTION_LOG_PARAM(BOOL, debug);
         FUNCTION_LOG_PARAM(STRING_LIST, packageList);
+        FUNCTION_LOG_PARAM(STRING_LIST, pgList);
+        FUNCTION_LOG_PARAM(STRING, pgBeta);
     FUNCTION_LOG_END();
 
     RenderData result = {.script = strNew(), .debug = debug};
@@ -177,13 +207,20 @@ vmRender(const VmDefinition *const vmDef, const bool debug, const StringList *co
         vmRenderTitle(&result, "Base container");
         strCatFmt(result.script, "FROM %s\n", strZ(vmDef->osImage));
 
-        // Packages
+        // Install packages
+        String *const packagePre = strNew();
+
         vmRenderTitle(&result, "Install packages");
 
         switch (vmDef->osType)
         {
             case osTypeDebian:
             {
+                vmRenderCommand(&result, "apt-get update");
+                strCatZ(
+                    packagePre,
+                    "export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive apt-get install -y"
+                    " --no-install-recommends");
 
                 break;
             }
@@ -195,8 +232,54 @@ vmRender(const VmDefinition *const vmDef, const bool debug, const StringList *co
                 break;
             }
         }
+
+        for (unsigned int packageIdx = 0; packageIdx < strLstSize(packageList); packageIdx++)
+            vmRenderCommand(&result, zNewFmt("%s %s", strZ(packagePre), strZ(strLstGet(packageList, packageIdx))));
+
+        // Install PostgreSQL packages
+        if (!strLstEmpty(pgList))
+        {
+            vmRenderTitle(&result, "Install PostgreSQL packages");
+
+            switch (vmDef->osType)
+            {
+                case osTypeDebian:
+                {
+                    vmRenderCommand(
+                        &result,
+                        strZ(
+                            strNewFmt(
+                                "echo \"deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -s -c)-pgdg main%s\" >>"
+                                " /etc/apt/sources.list.d/pgdg.list",
+                                pgBeta == NULL ? "" : zNewFmt(" %s", strZ(pgBeta)))));
+                    vmRenderCommand(
+                        &result, "wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -");
+                    vmRenderCommand(&result, "apt-get update");
+                    vmRenderCommand(&result, zNewFmt("%s postgresql-common libpq-dev", strZ(packagePre)));
+                    vmRenderCommand(
+                        &result,
+                        "sed -i 's/^\\#create\\_main\\_cluster.*$/create\\_main\\_cluster \\= false/'"
+                        " /etc/postgresql-common/createcluster.conf");
+
+                    break;
+                }
+
+                default:
+                {
+                    ASSERT(vmDef->osType == osTypeRhel);
+
+                    break;
+                }
+            }
+
+            for (unsigned int packageIdx = 0; packageIdx < strLstSize(packageList); packageIdx++)
+                vmRenderCommand(&result, zNewFmt("%s %s", strZ(packagePre), strZ(strLstGet(packageList, packageIdx))));
+        }
     }
     MEM_CONTEXT_TEMP_END();
+
+    // Line feed after last command
+    strCatChr(result.script, '\n');
 
     FUNCTION_LOG_RETURN(STRING, result.script);
 }
@@ -236,7 +319,7 @@ cmdVmBuild(const String *const vm)
 
     MEM_CONTEXT_TEMP_BEGIN()
     {
-        vmRender(vmDefinition(vm), false, vmPackageList(vmDefinition(vm)->osType));
+        vmRender(vmDefinition(vm), false, vmPackageList(vmDefinition(vm)->osType), strLstNew(), NULL);
     }
     MEM_CONTEXT_TEMP_END();
 
--- test/src/module/test/vmTest.c
+++ test/src/module/test/vmTest.c
@@ -16,16 +16,35 @@ testRun(void)
         // -------------------------------------------------------------------------------------------------------------------------
         TEST_TITLE("render debian");
 
+        #define TEST_DEBIAN_PACKAGE_INSTALL                                                                                        \
+            "export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends"
+
         StringList *packageList = strLstNew();
         strLstAddZ(packageList, "package1");
         strLstAddZ(packageList, "package2");
 
+        StringList *pgList = strLstNew();
+        strLstAddZ(pgList, "9.6");
+        strLstAddZ(pgList, "10");
+
         TEST_RESULT_STR_Z(
-            vmRender(vmDefinition(STRDEF("u22")), false, packageList),
+            vmRender(vmDefinition(STRDEF("u22")), false, packageList, pgList, STRDEF("16")),
             "# Base container\n"
             "FROM ubuntu:22.04\n"
             "\n"
-            "# Install packages\n",
+            "# Install packages\n"
+            "RUN apt-get update\n"
+            "RUN " TEST_DEBIAN_PACKAGE_INSTALL " package1\n"
+            "RUN " TEST_DEBIAN_PACKAGE_INSTALL " package2\n"
+            "\n"
+            "# Install PostgreSQL packages\n"
+            "RUN echo \"deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -s -c)-pgdg main 16\""
+            " >> /etc/apt/sources.list.d/pgdg.list\n"
+            "RUN wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -\n"
+            "RUN apt-get update\n"
+            "RUN " TEST_DEBIAN_PACKAGE_INSTALL " postgresql-common libpq-dev\n"
+            "RUN sed -i 's/^\\#create\\_main\\_cluster.*$/create\\_main\\_cluster \\= false/'"
+            " /etc/postgresql-common/createcluster.conf\n",
             "render");
     }
 
